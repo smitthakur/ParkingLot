d4544c9fcb02e913e97734b7429273e7980469bc
Smit Thakur <smitthakur@gmail.com>
2023-03-30 20:21:30 +0530

    Added base model

--- src/models/BaseModel.java
+++ src/models/BaseModel.java
@@ -1,5 +1,14 @@
 package models;
 
 public class BaseModel {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
